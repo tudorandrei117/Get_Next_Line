85d6cdb8ec71ffd98fe13eb4f329dcca8ec65644
Tudor-Andrei Burlacu <tburlacu@c1r15s9.42lisboa.com>
2022-12-05 16:57:37 +0000

    hehe

--- get_next_line.c
+++ get_next_line.c
@@ -5,65 +5,92 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: tburlacu <tburlacu@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2022/11/18 12:10:31 by tburlacu          #+#    #+#             */
-/*   Updated: 2022/11/24 14:38:32 by tburlacu         ###   ########.fr       */
+/*   Created: 2022/09/29 23:02:26 by pedperei          #+#    #+#             */
+/*   Updated: 2022/11/24 14:21:03 by tburlacu         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "get_next_line.h"
+#include "get_next_line_bonus.h"
+#include <fcntl.h>
 
-/*
- * Reads from a file descriptor, and returns a string
-  containing the contents of the file
- * 
- * @param fd file descriptor
- * @param buffer This is the buffer that will be used to read the file.
- * @param text the string that will be returned
- * 
- * @return a pointer to a string.
- */
-char	*ft_read(int fd, char *buffer, char *text)
+/*+2 por cuase do \n e do null terminator*/
+char	*ft_current_line(char *str)
 {
-	int		index;
-	char	*temp;
+	char	*line;
+	int		i;
 
-	index = 1;
-	temp = NULL;
-	while (index > 0)
+	line = NULL;
+	i = 0;
+	if (!(str) || str[i] == '\0')
+		return (NULL);
+	while (str[i] != '\n' && str[i] != '\0')
+		i++;
+	if (str[i] == '\0')
+		i--;
+	line = (char *)malloc(sizeof(char) * (i + 2));
+	if (!(line))
+		return (NULL);
+	i = 0;
+	while (str[i] != '\n' && str[i] != '\0')
 	{
-		index = read(fd, buffer, BUFFER_SIZE);
-		if (index < 0)
-		{
-			text = NULL;
-			index = '\0';
-			return (NULL);
-		}
-		buffer[index] = '\0';
-		temp = text;
-		text = ft_strjoin(temp, buffer);
-		free(temp);
-		if (ft_strchr(buffer, '\n') != '\0')
-			break ;
+		line[i] = str[i];
+		i++;
 	}
+	if (str[i] == '\n')
+		line[i++] = '\n';
+	line[i] = '\0';
+	return (line);
 }
 
-
-
-char *get_next_line(int fd)
+/*aloca-se memoria para a string atual (ft_strlen) 
+menos a linha que vai ser	retornada(i)*/
+char	*ft_next_line(char *str)
 {
-	
+	char	*new_line;
+	char	*temp;
+	int		i;
+
+	new_line = NULL;
+	temp = NULL;
+	if (!(str))
+		return (NULL);
+	if (ft_strchr_mod(str, '\n') != NULL)
+		temp = ft_strchr_mod(str, '\n');
+	else
+	{
+		free(str);
+		return (NULL);
+	}
+	new_line = (char *)malloc(sizeof(char) * (ft_strlen_mod(temp) + 1));
+	if (!(new_line))
+		return (NULL);
+	i = -1;
+	while (temp[++i] != '\0')
+		new_line[i] = temp[i];
+	new_line[i] = '\0';
+	free(str);
+	return (new_line);
 }
 
 
 
+char	*get_next_line(int fd)
+{
+	char		*buffer;
+	static char	*list;
+	char		*temp;
 
-
-
-
-
-
-
-
+	if (fd < 0 || fd > 4096 || BUFFER_SIZE <= 0)
+		return (NULL);
+	buffer = (char *)malloc(sizeof(char) * (BUFFER_SIZE + 1));
+	if (!(buffer))
+		return (NULL);
+	list = ft_read(fd, buffer, list);
+	temp = ft_current_line(list);
+	list = ft_next_line(list);
+	free(buffer);
+	return (temp);
+}
 
 int main(void)
 {
--- get_next_line.h
+++ get_next_line.h
@@ -5,25 +5,27 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: tburlacu <tburlacu@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2022/11/21 14:10:42 by tburlacu          #+#    #+#             */
-/*   Updated: 2022/11/24 13:52:42 by tburlacu         ###   ########.fr       */
+/*   Created: 2022/10/02 19:43:37 by pedperei          #+#    #+#             */
+/*   Updated: 2022/11/22 16:02:38 by tburlacu         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef GET_NEXT_LINE_H
 # define GET_NEXT_LINE_H
-
-# include <stdarg.h>
+# include <stdlib.h>
 # include <unistd.h>
+# include <stdio.h>
+
 
 # ifndef BUFFER_SIZE
 #  define BUFFER_SIZE 10
 # endif
 
-
 char	*get_next_line(int fd);
-int		ft_strlen(char *s);
+size_t	ft_strlen_mod(const char *s);
+char	*ft_strchr_mod(const char *str, int c);
+char	*ft_strdup(const char *s);
 char	*ft_strjoin(char const *s1, char const *s2);
-char	*ft_strchr(const char *s, int c);
+void	*ft_memset(void *ptr, int x, size_t n);
 
 #endif
